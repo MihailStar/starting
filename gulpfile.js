a6443030f2b7e19e08d4e74836d3b01835e55134
Михаил Александрович <mhlstr@mail.ru>
2017-07-28 23:20:42 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const gulp = require('gulp');
-
-require('require-dir')('./gulp', {
+require('require-dir')('./gulp/task', {
     recurse: true
 });
-
-gulp.task('build', gulp.series('sprite:vector', gulp.parallel(
-    'font',
-    'html',
-    'image',
-    'script:library',
-    'script:main',
-    'sprite:raster',
-    'style'
-)));
-
-gulp.task('default', gulp.series('clean', 'build', gulp.parallel(
-    'server',
-    'watch'
-)));
