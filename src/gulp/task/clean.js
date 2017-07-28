d9ba419aa3232a49bcb45d20bf95951761db8d83
Михаил Александрович <mhlstr@mail.ru>
2017-07-28 22:32:12 +0300

    Delete clean.js

--- src/gulp/task/clean.js
+++
@@ -1,8 +0,0 @@
-const configuration = require('../configuration');
-const del = require('del');
-const gulp = require('gulp');
-
-gulp.task('clean', (done) => {
-    del(configuration.root.output);
-    done();
-});
