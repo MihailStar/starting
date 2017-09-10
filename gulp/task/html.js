ff06d94e5bc29b53c57af3385b5b29ec7e0e8fc3
Михаил Александрович <mhlstr@mail.ru>
2017-09-10 21:53:03 +0300

    Add files via upload

--- gulp/task/html.js
+++ gulp/task/html.js
@@ -1,12 +1,23 @@
 const configuration = require('../configuration');
+const frontMatter = require('gulp-front-matter');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const rigger = require('gulp-rigger');
+const swig = require('gulp-swig');
 
 gulp.task('html', () => {
     return gulp
         .src(configuration.path.input.html)
         .pipe(rigger())
+        .pipe(frontMatter({
+            property: 'data',
+            remove: true
+        }))
+        .pipe(swig({
+            defaults: {
+                cache: false
+            }
+        }))
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true
