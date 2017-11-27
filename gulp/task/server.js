4b4ebacf489cbf6d5c315af5e10e83907719a2be
Михаил Александрович <mhlstr@mail.ru>
2017-11-27 23:02:40 +0300

    Add files via upload

--- gulp/task/server.js
+++ gulp/task/server.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 
 gulp.task('server', () => {
     browserSync.init({
-        // logPrefix: 'browser-syync',
+        logPrefix: 'browser-sync',
         notify: false,
         online: false,
         open: false,
