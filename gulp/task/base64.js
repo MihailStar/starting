5fce145691bac00f2a49114b681c53563eb83dca
Михаил Александрович <mhlstr@mail.ru>
2017-12-12 17:03:09 +0300

    Add files via upload

--- gulp/task/base64.js
+++ gulp/task/base64.js
@@ -1,10 +1,27 @@
 const configuration = require('../configuration');
 const gulp = require('gulp');
 const imageDataURI = require('gulp-image-data-uri');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('base64', () => {
     return gulp
         .src(configuration.path.input.base64)
+        .pipe(imagemin([
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 5
+            }),
+            imagemin.svgo({
+                plugins: [{
+                    cleanupIDs: false
+                }]
+            })
+        ]))
         .pipe(imageDataURI())
         .pipe(gulp.dest(configuration.path.output.base64));
 });
