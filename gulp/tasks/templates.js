49d49554eaa64b4fa88c9a37b2355332e7d3270b
MihailStar <mhlstr@mail.ru>
2018-12-31 08:20:56 +0300

    Вернул gulp-replace в templates таск

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -3,12 +3,14 @@ import gulp from 'gulp';
 import gulpIf from 'gulp-if';
 import prettier from 'gulp-prettier';
 import pug from 'gulp-pug';
+import replace from 'gulp-replace';
 import size from 'gulp-size';
 
 function compileTemplates() {
   return gulp
     .src(paths.templates.src)
     .pipe(pug())
+    .pipe(gulpIf(!isDevelopment, replace(/<\/script>/g, '</script>\n')))
     .pipe(gulpIf(!isDevelopment, prettier()))
     .pipe(gulpIf(!isDevelopment, size({
       title: 'compileTemplates size'
