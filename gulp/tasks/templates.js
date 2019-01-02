6198fd19235a69d8d4d8c051750092f2a25ddb23
MihailStar <mhlstr@mail.ru>
2019-01-02 20:20:05 +0300

    Добавил prettier magic comments

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -8,11 +8,14 @@ import size from 'gulp-size';
 import { paths, isDevelopment } from '../configuration';
 
 function compileTemplates() {
+  // prettier magic comments
+  const regExp = /\n* *<!-- *display: *(?:block|inline) *-->/g;
+
   return gulp
     .src(paths.templates.src)
     .pipe(pug())
-    .pipe(gulpIf(!isDevelopment, replace(/<\/script>/g, '</script>\n')))
     .pipe(gulpIf(!isDevelopment, prettier()))
+    .pipe(gulpIf(!isDevelopment, replace(regExp, '')))
     .pipe(gulpIf(!isDevelopment, size({
       title: 'compileTemplates',
     })))
