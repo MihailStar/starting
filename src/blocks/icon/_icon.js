078c84bc35c178219f5c01a3784e16f5cf1d8b10
MihailStar <mhlstr@mail.ru>
2019-08-28 14:40:51 +0300

    Рефакторит миксин icon

--- src/blocks/icon/_icon.js
+++
@@ -1,3 +0,0 @@
-import svg4everybody from '../../../node_modules/svg4everybody/dist/svg4everybody.min';
-
-svg4everybody();
---
+++ src/blocks/icon/icon.js
@@ -0,0 +1,3 @@
+import svg4everybody from 'svg4everybody/dist/svg4everybody.min';
+
+svg4everybody();
