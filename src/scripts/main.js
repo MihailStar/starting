59dcb86452239e681341923f51d7a14a108a0a40
MihailStar <mhlstr@mail.ru>
2020-02-02 08:13:01 +0300

    feat: подключает скрипты блокa/миксина сounter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,12 @@
 import './utilities';
+
 import '../blocks/icon/icon';
+import Counter from '../blocks/counter/counter';
+
+if (window.NodeList !== undefined && NodeList.prototype.forEach === undefined) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+document
+  .querySelectorAll('.starting .counter')
+  .forEach((counter) => new Counter(counter));
