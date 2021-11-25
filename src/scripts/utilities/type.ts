3c507aedfbb16cb42d75d29f2d440c91b2bbe1f4
MihailStar <mhlstr@mail.ru>
2021-11-25 20:55:04 +0300

    feat: добавляет утилиты Для работы с типами

---
+++ src/scripts/utilities/type.ts
@@ -0,0 +1,17 @@
+type DeepPartial<O> = {
+  [K in keyof O]?: O[K] extends object ? DeepPartial<O[K]> : O[K];
+};
+
+type ExtractArrayType<A> = A extends Array<infer U> ? U : never;
+
+type FilterKeysByType<O, T> = {
+  [K in keyof O]: O[K] extends T ? K : never;
+}[keyof O];
+
+type Keys<O> = keyof O;
+
+type NonReadonly<O> = {
+  -readonly [K in keyof O]?: O[K];
+};
+
+export { DeepPartial, ExtractArrayType, FilterKeysByType, Keys, NonReadonly };
