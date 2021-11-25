3aa2967575d45308210609b15638e32775cfcbd9
MihailStar <mhlstr@mail.ru>
2021-11-25 20:10:15 +0300

    refactor

--- src/scripts/utilities/getRandomInt.ts
+++ src/scripts/utilities/getRandomInt.ts
@@ -1,5 +1,6 @@
 /**
  * @returns [min, max]
+ * @example getRandomInt(0, array.length - 1);
  */
 function getRandomInt(min: number, max: number): number {
   const randomInt = Math.floor(min + Math.random() * (max + 1 - min));
