bf95a02ca0e377a3a8febdc497751af7d00f48e9
MihailStar <mhlstr@mail.ru>
2021-12-13 01:20:19 +0300

    feat: добавляет утилиту

---
+++ src/scripts/utilities/caching-decorator.ts
@@ -0,0 +1,23 @@
+/* eslint-disable func-names */
+
+function cachingDecorator<Arg, Val>(
+  func: (argument: Arg) => Val
+): (arg: Arg) => Val {
+  const cache = new Map<Arg, Val>();
+
+  return function (arg: Arg): Val {
+    const value = cache.get(arg);
+
+    if (value !== undefined) {
+      return value;
+    }
+
+    const result = func(arg);
+
+    cache.set(arg, result);
+
+    return result;
+  };
+}
+
+export { cachingDecorator };
