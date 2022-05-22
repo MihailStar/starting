dd6e8782ee86aa23f5444000f7fb8bdaa930451f
MihailStar <mhlstr@mail.ru>
2022-05-22 19:55:40 +0300

    fix: расширяет утилиту

--- src/scripts/utilities/type.ts
+++ src/scripts/utilities/type.ts
@@ -2,7 +2,11 @@ type DeepPartial<Obj> = {
   [K in keyof Obj]+?: Obj[K] extends object ? DeepPartial<Obj[K]> : Obj[K];
 };
 
-type ExtractArrayType<Arr> = Arr extends Array<infer U> ? U : never;
+// type ExtractArrayType<Arr> = Arr[Exclude<keyof Arr, keyof []>];
+// type ExtractArrayType<Arr> = Arr[Extract<keyof Arr, number>];
+type ExtractArrayType<Arr> = Arr extends Array<infer U> | ReadonlyArray<infer U>
+  ? U
+  : never;
 
 type FilterKeysByType<Obj, Type> = {
   [K in keyof Obj]: Obj[K] extends Type ? K : never;
