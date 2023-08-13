f1df4b5d7077548ad76b1713d7014a42c1991c1c
MihailStar <mhlstr@mail.ru>
2023-08-13 13:05:10 +0300

    feat: добавляет утилиту

---
+++ src/scripts/utilities/exhaustive-check.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * Проверяет на необработанные `case` в `switch`
+ */
+function exhaustiveCheck(value: never): void {}
+
+export { exhaustiveCheck };
